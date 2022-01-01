a581b92825888a9fc05ec16f83e01758dafdcc43
otwoma <otwomadennis365@gmail.com>
2022-01-01 22:05:48 +0300

    added more parts to the webaage

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,23 @@
 const app = () => {
     const burger = document.querySelector('.burger');
-    const navList = document.querySelector('.header__navlist')
+    const navList = document.querySelector('.header__navlist');
+    const blogsBtnPrev = document.querySelector('.blogs__btn--prev');
+    const blogsBtnNext = document.querySelector('.blogs__btn--next');
+    const blogsContainer = document.querySelector('.blogs__cards');
     burger.addEventListener('click', () => {
         navList.classList.toggle('nav-active');
         burger.classList.toggle('toggle');
+    })
+    // function to handle scrolling
+    const scrollElement = (element, scrollDis) => {
+        element.scrollBy({left: scrollDis, behavior: "smooth"})
+    }
+    // handling the scrolling for the blogs container holding the cards
+    blogsBtnPrev.addEventListener('click', () => {
+        scrollElement(blogsContainer, -6000)
+    })
+    blogsBtnNext.addEventListener('click', () => {
+        scrollElement(blogsContainer, 6000)
     })
 }
 app()
